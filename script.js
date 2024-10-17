d63a0ffffb2ce82f6f2bbb933371c410261f79be
gfredex <g.fredex@gmail.com>
2024-10-17 16:50:09 +0300

    add: interim solution second task

--- script.js
+++ script.js
@@ -126,3 +126,48 @@ class MyDropDown extends HTMLElement {
 }
 
 customElements.define('my-drop-down', MyDropDown); /* здесь указываем как будет называться тэг кастомного элемента и откуда будет брать вид и логику*/
+
+class CustomTooltip extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.innerHTML = `
+        <style>
+        .tooltip {
+            position: absolute;
+            left: 50%;
+            top: -150%;
+            padding: 5px;
+            font-style: italic;
+            display: none;
+             border: 1px solid #000;
+        }
+
+        .tooltip.show {
+            display: inline-block;           
+        }
+            
+    </style>
+    <span class="tooltip"></span>
+        `;
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+    render() {
+        const text = this.getAttribute('text');
+        const color = this.getAttribute('color');
+        const bgColor = this.getAttribute('background')
+        const toolTip = this.shadowRoot.querySelector('.tooltip');
+        toolTip.textContent = text;
+        toolTip.style.color = color || '#fff';
+        toolTip.style.background = bgColor || '#333333';
+        const parent = toolTip.parentNode;
+
+        parent.addEventListener('click', e => {
+            console.log(e);
+        })
+    }
+}
+customElements.define('custom-tool-tip', CustomTooltip);
